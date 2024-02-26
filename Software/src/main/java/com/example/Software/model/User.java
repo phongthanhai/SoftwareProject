04c7caaa5e113d70ff800ecfd4ccebbee6da86ef
admin <admin@LAPTOP-KDLVVB9O>
2024-02-26 23:07:09 +0700

    a new feature branch to construct user

--- Software/src/main/java/com/example/Software/model/User.java
+++ Software/src/main/java/com/example/Software/model/User.java
@@ -1,4 +1,48 @@
 package com.example.Software.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.validator.constraints.CodePointLength;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mobile")
+    private String mobile;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "registered_at")
+    private Date registeredAt;
+
+    @Column(name = "last_login")
+    private Date lastLogin;
+
+    @Column(name = "intro")
+    private String intro;
+
+    @Column(name = "profile")
+    private String profile;
 }
