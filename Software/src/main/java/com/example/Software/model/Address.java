abd8877e96a29029684c0ca5a628bfd0dfbb1bec
tuanvo1603 <tuan.vv210905@sis.hust.edu.vn>
2024-02-28 22:07:03 +0700

    Init cart model

---
+++ Software/src/main/java/com/example/Software/model/Address.java
@@ -0,0 +1,18 @@
+package com.example.Software.model;
+
+public class Address {
+
+    private String country;
+
+    private String city;
+
+    private String district;
+
+    private String commune;
+
+    private String detail;
+
+    private String phoneNumber;
+
+    private String email;
+}
---
+++ Software/src/main/java/com/example/Software/model/CartItem.java
@@ -0,0 +1,20 @@
+package com.example.Software.model;
+
+import com.example.Software.constant.ShoeColor;
+
+import java.util.List;
+
+public class CartItem {
+
+    private String productId;
+
+    private String name;
+
+    private Integer quantity;
+
+    private List<ShoeColor> colors;
+
+    private String size;
+
+    private Float price;
+}
