a87de05e7ed188e280a5f19d45eb47ed25a212d9
admin <admin@LAPTOP-KDLVVB9O>
2024-04-03 01:08:36 +0700

    update jwt service

--- Software/src/main/java/com/example/Software/service/jwt/impl/JWTServiceImpl.java
+++ Software/src/main/java/com/example/Software/service/jwt/impl/JWTServiceImpl.java
@@ -1,24 +1,41 @@
 package com.example.Software.service.jwt.impl;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JWTServiceImpl {
     private String generateToken(UserDetails userDetails){
         return Jwts.builder().setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60))
                 .signWith(getSiginKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+    private String extractUserName(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsResolvers){
+        final Claims claims = extractALlClaims(token);
+        return claimsResolvers.apply(claims);
+    }
 
     private Key getSiginKey(){
+        byte[] key = Decoders.BASE64.decode("");
+        return Keys.hmacShaKeyFor(key);
+    }
 
+    private Claims extractALlClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSiginKey()).build().parseClaimsJwt(token).getBody();
     }
 }
