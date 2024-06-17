84a4a6f5b4daab0826c7031c6baba9763eb86f40
admin <admin@LAPTOP-KDLVVB9O>
2024-06-17 21:19:47 +0700

    update product

--- Software/src/main/java/com/example/Software/model/Image.java
+++ Software/src/main/java/com/example/Software/model/Image.java
@@ -2,8 +2,10 @@ package com.example.Software.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Image {
     private String original;
