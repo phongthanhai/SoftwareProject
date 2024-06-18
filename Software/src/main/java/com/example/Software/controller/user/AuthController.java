8389069d4e345ade2ef04fa644f9bf4a414c4be0
minh.pham2 <minh.pham2@ntq-solution.com.vn>
2024-06-18 11:01:13 +0700

    Update check email

--- Software/src/main/java/com/example/Software/controller/user/AuthController.java
+++ Software/src/main/java/com/example/Software/controller/user/AuthController.java
@@ -5,17 +5,25 @@ import com.example.Software.request.user.LoginRequest;
 import com.example.Software.request.user.UserDetailRequest;
 import com.example.Software.response.jwt.JwtAuthenticationResponse;
 import com.example.Software.service.user.AuthService;
+import com.example.Software.service.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    UserService userService;
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody UserDetailRequest userDetailRequest){
+        String userEmail = userDetailRequest.getEmail();
+        User user = userService.getUserByEmail(userEmail);
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
         return ResponseEntity.ok(authService.signup(userDetailRequest));
     }
 
